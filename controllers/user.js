836f809358db023919a4106e6192f90c92526997
Asim Zaidi <asimzaidih@gmail.com>
2018-10-18 05:39:16 -0700

    signin consistency, playing with credential bug, added partials

--- controllers/user.js
+++ controllers/user.js
@@ -1,27 +1,32 @@
-const User = require('../models/newUser');
+const User = require('../models/user');
 
 module.exports = function (app) {
 
+    // GET HOME (INDEX)
     app.get('/', (req, res) => {
-        res.render('signUp');
+        res.render('home');
     });
-
-    app.post('/users/', (req, res) => {
-        const email = req.body.email;
-        const password = req.body.password;
-        User.signUp(email, password)
-        .then(userCredential => User.signInWithCredential(userCredential.credential))
-        .then(user => res.render('dashboard', { email: user.email }))
-        .catch(console.error);
-    });
-    app.post('/login/', (req, res) => {
+    // POST login from index
+    app.post('/', (req, res) => {
         User.signInWithEmailAndPassword(req.body.email, req.body.password)
         .then(userCredential => {
             res.render('dashboard', { email: userCredential.user.email });
         }).catch(console.error);
     });
-    app.get('/login/', (req, res) => {
-        res.render('login');
+
+    // GET / Show Signup
+    app.get('/signup/', (req, res) => {
+        res.render('signup');
+    });
+    // POST USER then sign user in (broken) why /users/?
+    app.post('/users/', (req, res) => {
+        const email = req.body.email;
+        const password = req.body.password;
+        User.signup(email, password)
+        .then(userCredential => User.signInWithCredential(userCredential.credential))
+        // unfortunately after sign up, sign in won't follow so we'll do  it the long way.
+        .then(user => res.render('dashboard', { email: user.email }))
+        .catch(console.error);
     });
 
 }
--- models/user.js
+++ models/user.js
@@ -1,61 +1,20 @@
-const firebase = require('./firebase');
+const auth = require('./firebase').auth;
 
-var uiConfig = {
-    callbacks: {
-        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-          // User successfully signed in.
-          // Return type determines whether we continue the redirect automatically
-          // or whether we leave that to developer to handle.
-          return true;
-        },
-        uiShown: function() {
-          // The widget is rendered.
-          // Hide the loader.
-          document.getElementById('loader').style.display = 'none';
-        }
-      },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: '/dashboard',
-    signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
-        provider = firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        requireDisplayNam = true
-    ],
-    // tosUrl and privacyPolicyUrl accept either url string or a callback function. Terms of service url/callback.
-    tosUrl: '/terms',
-    // Privacy policy url/callback.
-    privacyPolicyUrl: function() {
-        window.location.assign('By signing up you agree to our Terms and Conditions.');
+module.exports = (function() {
+    function signup(email, password) {
+        return auth.createUserWithEmailAndPassword(email, password);
     }
-};
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
+    function signInWithCredential(credential) {
+        return auth.signInWithCredential(credential);
+    }
+    function signInWithEmailAndPassword(email, password) {
+        return auth.signInWithEmailAndPassword(email, password);
+    }
 
-// auth.createUserWithEmailAndPassword(email, password).catch(function(error) {
-//     // Handle Errors here.
-//     var errorCode = error.code;
-//     var errorMessage = error.message;
-//     // ...
-// });
-
-// firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-//     // Handle Errors here.
-//     var errorCode = error.code;
-//     var errorMessage = error.message;
-//     // ...
-//   });
-
-// firebase.auth().onAuthStateChanged(function(user) {
-//     if (user) {
-//         // set html to display user...
-//     }
-//     else {
-//         console.log("no user signed in");
-        
-//     }
-// })
-// FirebaseUI config.
+    return {
+        signup: signup,
+        signInWithCredential: signInWithCredential,
+        signInWithEmailAndPassword: signInWithEmailAndPassword
+    }
+})();
+// Why do we use this () at the end again? It just tells the code to call the function?
